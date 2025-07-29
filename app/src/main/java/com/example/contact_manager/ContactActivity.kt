ad0fc84649f00007033c88903a79ccc05b8266ab
sylviaehab <silvyaehab@gmail.com>
2025-07-29 16:28:12 +0300

    second task

--- app/src/main/java/com/example/contact_manager/ContactActivity.kt
+++ app/src/main/java/com/example/contact_manager/ContactActivity.kt
@@ -1,6 +1,9 @@
 package com.example.contact_manager
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,9 +17,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.contact_manager.ui.theme.Contact_ManagerTheme
 
 class ContactActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+        val btnMain=findViewById<Button>(R.id.btnMain)
+        btnMain.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
 
